da8034cbceca930764a753e728f5f4f16e83b30d
Etherisium <zarin.sumaiya@gmail.com>
2024-04-26 14:52:21 +0600

    5

--- final.js
+++ final.js
@@ -1,3 +1,5 @@
+var allMealdata = [];
+
 function connect() {
     var searchmeal = document.getElementById("searchbox").value;
     var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchmeal}`;
@@ -11,12 +13,43 @@ function show(data) {
     var oldContent = document.getElementById("container2");
     oldContent.textContent = ""; 
 
-for (var i = 0; i < Math.min(data.length, 5); i++) {
+    for (var i = 0; i < Math.min(data.length, 5); i++) {
         var newDiv = document.createElement("div");
-        newDiv.innerHTML = `<h3><b>Meal Name: ${data[i].strMeal}</b></h3>
-        <img src="${data[i].strMealThumb}" alt="${data[i].strMeal}"> <br>
-        <p2><b>Meal ID: ${data[i].idMeal}</b></p2>
-        <h3>Category: ${data[i].strCategory}</h3> <br> <p3>Instructions: ${data[i].strInstructions}</p3> `;
+        newDiv.innerHTML = `
+                            <h3><b>Meal Name: ${data[i].strMeal}</b></h3>
+                            <img src="${data[i].strMealThumb}" alt="${data[i].strMeal}"> <br>
+                            <p2><b>Meal ID: ${data[i].idMeal}</b></p2>
+                            <h3>Category: ${data[i].strCategory}</h3> <br> <p3>Instructions: ${data[i].strInstructions}</p3>
+                            `;
+
+        newDiv.classList.add("innerStyle");
+        oldContent.appendChild(newDiv);
+    }
+
+    if (data.length > 5) {
+        allMealdata = data;
+        var showAllBtn = document.createElement("buttonone");
+        showAllBtn.textContent = "Show All";
+        showAllBtn.classList.add("buttonone");
+        showAllBtn.onclick = function() {
+            display(allMealdata);
+        };
+        oldContent.appendChild(showAllBtn);
+    }
+}
+
+function display(allMealdata) {
+    var oldContent = document.getElementById("container2");
+    oldContent.textContent = "";
+
+    for (var i = 0; i < allMealdata.length; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `<h3><b>Meal ID: ${allMealdata[i].idMeal}</b></h3>
+                            <h3><b>Meal Name: ${allMealdata[i].strMeal}</b></h3>
+                            <img src="${allMealdata[i].strMealThumb}" alt="${allMealdata[i].strMeal}"><br>
+                            <p2><b>Meal ID: ${allMealdata[i].idMeal}</b></p2>
+                            <h3>Category: ${allMealdata[i].strCategory}</h3>
+                            `;
 
         newDiv.classList.add("innerStyle");
         oldContent.appendChild(newDiv);
