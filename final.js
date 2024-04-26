38e936263dff47c84eef5ff48795ac7cfc94f6bd
Etherisium <zarin.sumaiya@gmail.com>
2024-04-26 15:14:16 +0600

    6

--- final.js
+++ final.js
@@ -18,8 +18,10 @@ function show(data) {
         newDiv.innerHTML = `
                             <h3><b>Meal Name: ${data[i].strMeal}</b></h3>
                             <img src="${data[i].strMealThumb}" alt="${data[i].strMeal}"> <br>
-                            <p2><b>Meal ID: ${data[i].idMeal}</b></p2>
-                            <h3>Category: ${data[i].strCategory}</h3> <br> <p3>Instructions: ${data[i].strInstructions}</p3>
+                            <p2><b>Meal ID: ${data[i].idMeal}</b></p2><br>
+                            <h3>Category: ${data[i].strCategory}</h3> <br>
+                            <h3>Meal Area: ${data[i].strArea}</h3> <br>
+                            <p3>Instructions: ${data[i].strInstructions}</p3>
                             `;
 
         newDiv.classList.add("innerStyle");
@@ -44,11 +46,11 @@ function display(allMealdata) {
 
     for (var i = 0; i < allMealdata.length; i++) {
         var newDiv = document.createElement("div");
-        newDiv.innerHTML = `<h3><b>Meal ID: ${allMealdata[i].idMeal}</b></h3>
-                            <h3><b>Meal Name: ${allMealdata[i].strMeal}</b></h3>
-                            <img src="${allMealdata[i].strMealThumb}" alt="${allMealdata[i].strMeal}"><br>
-                            <p2><b>Meal ID: ${allMealdata[i].idMeal}</b></p2>
-                            <h3>Category: ${allMealdata[i].strCategory}</h3>
+        newDiv.innerHTML = `<h3><b>Meal Name: ${allMealdata[i].strMeal}</b></h3>
+         <img src="${allMealdata[i].strMealThumb}" alt="${allMealdata[i].strMeal}"><br>
+        <p2><b>Meal ID: ${allMealdata[i].idMeal}</b></p2>
+        <h3>Meal Area: ${allMealdata[i].strArea}</h3> <br>
+        <h3>Category: ${allMealdata[i].strCategory}</h3>
                             `;
 
         newDiv.classList.add("innerStyle");
