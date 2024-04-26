b094ee662e2a25fbfe2249b9c52ace571da0a013
Etherisium <zarin.sumaiya@gmail.com>
2024-04-26 14:10:05 +0600

    3

--- final.js
+++ final.js
@@ -1,1 +1,9 @@
+function connect() {
+    var searchmeal = document.getElementById("searchbox").value;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchmeal}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => show(data.meals));
+}
 
